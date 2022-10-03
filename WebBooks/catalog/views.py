933370775d96321c2d3aad79169f94b5f689e7d4
TarumK <kyabishev72@gmail.com>
2022-10-03 13:58:37 +0300

    03102022

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+
+    data1 = datetime.today()
+    return render(request, "index.html", locals())
     # return HttpResponse('<h1>Главная страница сайта Мир книг!</h1>')
